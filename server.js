cec5bc441e1c7b740d2b4d0c0adac9f8730d48e1
KostasMateer <komateer@davidson.edu>
2023-03-20 18:41:11 -0400

    hello

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const database = require("./src/database.js");
 const app = express();
 const cors = require('cors');
-const port = 8000;
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.static(path.join(__dirname, "public")));
