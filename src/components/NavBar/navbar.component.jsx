e8ac0e8607a4ab71762d39ddf3812df7832576de
KostasMateer <komateer@davidson.edu>
2023-03-20 21:07:18 -0400

    updates

--- src/components/NavBar/navbar.component.jsx
+++ src/components/NavBar/navbar.component.jsx
@@ -5,8 +5,8 @@ function Navbar({image, title}) {
     return (
         <Grid className="navbar-color" sx={{position: 'sticky', top:-20}} container spacing={2}>
             <Grid item xs={1}></Grid>
-            <Grid item xs={1}>
-                <img className="center-image" height={50} width={50} src={image} alt="" />
+            <Grid sx={{justifyContent: 'left'}} item xs={1}>
+                <img className="center-image" style={{ maxHeight: '60px', maxWidth: '60px' }} src={image} alt="" />
             </Grid>
             <Grid item xs={10}>
                 <Typography className="center-text" variant="h4" align="left">{title}</Typography>
