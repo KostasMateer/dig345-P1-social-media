976845ecef6e0adf51ff7997c8c42d73aeed07b6
KostasMateer <komateer@davidson.edu>
2023-03-21 13:23:28 -0400

    final update

--- src/components/NavBar/navbar.component.jsx
+++ src/components/NavBar/navbar.component.jsx
@@ -1,23 +1,13 @@
-import { Typography, Grid } from "@mui/material"
+import { Typography, Stack } from "@mui/material"
 import "./navbar.style.css"
 
 function Navbar({image, title}) {
     return (
-        <Grid className="navbar-color" sx={{position: 'sticky', top:-20}} container spacing={2}>
-            <Grid item xs={4} sm={3} md={2}>
-                <img className="center-image" style={{ maxHeight: '60px', maxWidth: '60px' }} src={image} alt="" />
-            </Grid>
-            <Grid item sm={9} md={10} sx={{display: {'xs': 'none', 'sm': 'flex', 'md' : 'flex', 'lg' : 'none'}}}
-             style={{paddingLeft: window.innerWidth >= 603 && window.innerWidth <= 900 ?'0rem':'1rem'}}>
-                <Typography sx={{textAlign:'left'}} className="center-text" variant="h4">{title}</Typography>
-            </Grid>
-            <Grid item sm={9} md={9} sx={{display: {'xs': 'none', 'sm': 'none', 'md': 'none', 'lg':'flex'}, paddingLeft: '1'}}>
-                <Typography sx={{textAlign:'left'}} className="center-text" variant="h4">{title}</Typography>
-            </Grid>
-            <Grid item xs={8} sx={{display: {'xs': 'flex', 'sm': 'none'}}} style={{paddingLeft: '1.3rem'}}>
-                <Typography sx={{textAlign:'left'}} className="center-text" variant="h6">{title}</Typography>
-            </Grid>
-        </Grid>
+        <Stack className="navbar-color" direction="row" spacing={2}>
+            <img className="center-image" style={{ maxHeight: '60px', maxWidth: '60px' }} src={image} alt="logo of website" />
+            <Typography sx={{display: {"xs":"none", "sm":"flex"}}} className="center-text" variant="h5">{title}</Typography>
+            <Typography sx={{display: {"xs":"flex", "sm":"none"}}} className="center-text" variant="h6">{title}</Typography>
+        </Stack>
     );
 }
 
